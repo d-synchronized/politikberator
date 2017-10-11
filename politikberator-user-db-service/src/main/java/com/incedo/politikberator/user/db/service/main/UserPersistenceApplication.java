0b1aafca972aec9ed99a83e6eea4583787fccb81
dishant.anand <dishant.anand@T-248-602143L.ibtechnology.com>
2017-10-11 14:35:49 +0530

    Committing user database service

---
+++ politikberator-user-db-service/src/main/java/com/incedo/politikberator/user/db/service/main/UserPersistenceApplication.java
@@ -0,0 +1,15 @@
+package com.incedo.politikberator.user.db.service.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@EntityScan("com.incedo.politikberator.orm.*")
+@SpringBootApplication(scanBasePackages = "com.incedo.politikberator.*")
+public class UserPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPersistenceApplication.class, args);
+	}
+
+}
