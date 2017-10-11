a6e4351992c3f2156270167b8e08dfd0b5d1aee7
dishant.anand <d.synchronized@gmail.com>
2017-10-11 17:08:57 +0530

    Committing code to add Dozer Mapper in policy service, policy db service , user db service

---
+++ politikberator-user-db-service/src/main/java/com/incedo/politikberator/user/db/service/configuration/MappingConfiguration.java
@@ -0,0 +1,20 @@
+package com.incedo.politikberator.user.db.service.configuration;
+
+import org.dozer.spring.DozerBeanMapperFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+@Configuration
+public class MappingConfiguration {
+	
+	@Bean
+    public DozerBeanMapperFactoryBean dozerBeanMapperFactoryBean(@Value("classpath*:mapping/dozer-user-db-configuration.xml") Resource[] resources) throws Exception {
+        final DozerBeanMapperFactoryBean dozerBeanMapperFactoryBean = new DozerBeanMapperFactoryBean();
+        dozerBeanMapperFactoryBean.setMappingFiles(resources);
+        return dozerBeanMapperFactoryBean;
+    }
+
+}
+
