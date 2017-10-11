ad3f2d8ef16633bec605937f0b80a72ea6a499ca
rajul.shukla <rajul.shukla@incedoinc.com>
2017-10-11 11:57:05 +0530

    Adding api constant in common

---
+++ politikberator-common/src/main/java/com/incedo/politikberator/common/api/constant/ApiConstant.java
@@ -0,0 +1,9 @@
+package com.incedo.politikberator.common.api.constant;
+
+public interface ApiConstant {
+
+	public static final String API_BASE_URI = "/api";
+
+	public static final String API_VERSION = "/v1";
+
+}
